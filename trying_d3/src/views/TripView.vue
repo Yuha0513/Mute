624cd6bef99c2f048953b32e11e8a95a738cfd65
Yuha <jyuha759@gmail.com>
2022-11-16 17:53:22 +0900

    d3try

---
+++ trying_d3/src/views/TripView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>TripView</h1>
+    <WorldMap/>
+  </div>
+</template>
+
+<script>
+import WorldMap from '@/components/WorldMap'
+
+export default {
+  name: 'TripView',
+  components: {
+    WorldMap,
+  }
+}
+</script>
+
+<style>
+
+</style>
