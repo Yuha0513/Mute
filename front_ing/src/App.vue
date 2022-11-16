3c35f7524f13a9a7f6a1ad57b0d2c337f5dc2291
sejin <tpwls86@naver.com>
2022-11-16 17:45:40 +0900

    ..logout

---
+++ front_ing/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link :to="{ name: 'movies' }">movie</router-link> |
+      <router-link :to="{ name: 'trip' }">trip</router-link> |
+      <router-link :to="{ name: 'user' }">user</router-link>  |
+      <router-link v-if="isLogin === false" :to="{ name: 'signup' }">회원가입</router-link>  |
+      <router-link v-if="isLogin === false" :to="{ name: 'login'}">LOGIN</router-link>  |
+      <button v-if="isLogin === true" @click="logout">logout</button>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+<script>
+  export default {
+    methods: {
+      logout() {
+        this.$store.dispatch('logout')
+      } 
+    },
+    computed: {
+      isLogin() {
+        return this.$store.state.isLogined // 로그인 버젼 구현 아직 2022.11.16
+      }
+    }
+  }
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ front_ing/src/views/LoginView.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h3>Login</h3>
+        <form @submit.prevent>
+            <label for="username">아이디 : </label>
+            <input type="text" id="username" v-model="username">
+
+            <label for="password">비밀번호 : </label>
+            <input type="password" id="password" v-model="password">
+
+            <input type="submit" value="logIn" @click="logIn">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+    data() {
+        return {
+            username: null,
+            password: null,
+        }
+    },
+    methods: {
+        logIn() {
+            const username = this.username
+            const password = this.password
+
+            const payload = {
+                username: username,
+                password: password,
+            }
+            this.$store.dispatch('logIn', payload)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
