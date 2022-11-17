16b7e127ee2110dd9d03cab5f494fc9a2548e98a
Yuha <jyuha759@gmail.com>
2022-11-17 15:36:03 +0900

    11171536

---
+++ together_1117/back_ing/movies/serializers.py
@@ -0,0 +1,113 @@
+from rest_framework import serializers
+from .models import Movie, Actor, Review, ReviewComment, Community, Comment
+
+# 배우 목록
+class ActorListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Actor
+        fields = '__all__' 
+
+# 영화 목록
+class MovieListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+# 리뷰
+class ReviewSerializer(serializers.ModelSerializer):
+    class MovieSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('title',)
+    movie = MovieSerializer(read_only=True)
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+# 영화         
+class MovieSerializer(serializers.ModelSerializer):
+    class ActorSerializer(serializers.ModelSerializer):
+        class Meta:
+            model=Actor
+            fields = ('name',)
+    actors = ActorSerializer(many=True, read_only=True)
+
+    class ReviewSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Review
+            fields = ('title', 'content')
+    review_set = ReviewSerializer(many=True, read_only=True)
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+# 배우
+class ActorSerializer(serializers.ModelSerializer):
+    class MovieSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Movie
+            fields = ('title',)
+    movies = MovieSerializer(many=True, read_only=True,)
+    class Meta:
+        model = Actor
+        fields = '__all__'
+
+# 리뷰 목록
+class ReviewListSerializer(serializers.ModelSerializer):
+
+  # 리뷰가 달린 영화의 이름
+  movie_title = serializers.SerializerMethodField()
+
+  def get_movie_title(self, obj):
+    return obj.movie.title
+
+  # 유저 이름
+  userName = serializers.SerializerMethodField()
+
+  def get_userName(self,obj):
+    return obj.user.username
+
+  class Meta:
+    model = Review
+    fields = ('movie_title', 'userName', 'title','content', 'user',)
+    read_only_fileds = ('user',)
+
+# 리뷰글의 댓글
+class ReviewCommentSerializer(serializers.ModelSerializer):
+  userName = serializers.SerializerMethodField()
+  
+  def get_userName(self,obj):
+    return obj.user.username
+
+  class Meta:
+    model = ReviewComment
+    fields = ('id', 'userName', 'user', 'content', 'review', 'rank', 'created_at', 'updated_at',)
+    read_only_fields = ('user', 'review',)
+
+# 커뮤니티
+class CommunityListSerializer(serializers.ModelSerializer):
+
+  # 유저 이름
+  userName = serializers.SerializerMethodField()
+
+  def get_userName(self,obj):
+    return obj.user.username
+
+  class Meta:
+    model = Community
+    fields = ('id', 'userName', 'user', 'title', 'content', 'created_at', 'updated_at',)
+    read_only_fields = ('user',)
+
+# 코멘트
+class CommentSerializer(serializers.ModelSerializer):
+
+  # 유저 이름
+  userName = serializers.SerializerMethodField()
+  
+  def get_userName(self,obj):
+    return obj.user.username
+
+  class Meta:
+    model = Comment
+    fields = ('id', 'userName', 'user', 'content', 'created_at', 'updated_at', 'community',)
+    read_only_fields = ('user','community',)
---
+++ together_1117/front_2day/src/views/LoginView.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container">
+        <div class="main">
+            <h3 class="logo">Login</h3>
+            <form @submit.prevent class="container">
+                <input type="text" id="username" v-model="username" class="account" placeholder="아이디">                
+                <input type="password" id="password" v-model="password" class="account" placeholder="비밀번호">
+                <input type="submit" class='account' id="login" value="logIn" @click="logIn ">
+                <p class="suggest">Don't Have an Account? <router-link :to="{ name: 'signup'}">signup</router-link></p>
+            </form>
+
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'LoginView',
+    data() {
+        return {
+            username: null,
+            password: null,
+        }
+    },
+    methods: {
+        logIn() {
+            const username = this.username
+            const password = this.password
+
+            const payload = {
+                username: username,
+                password: password,
+            }
+            this.$store.dispatch('logIn', payload)
+        }
+    }
+}
+</script>
+
+<style>
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.main {
+    margin-top: 30px;
+    width: 350px;
+    height: 400px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+}
+
+.logo {
+    margin-top: 0px;
+    margin-bottom: 40px;
+}
+
+#login {
+    width: 100%;
+    background-color: rgb(93, 216, 169);
+    border-color: transparent;
+    color: white;
+    margin-top: 20px;
+}
+
+.account {
+    display: block;
+    margin-bottom: 3px;
+    padding: 3px;
+    border: 1px solid lightgray;
+    border-radius: 3px;
+    margin-bottom: 10px;
+}
+.suggest {
+    color: gray;
+    font-size: small;
+}
+</style>
